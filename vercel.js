ce1f0c845ed271771e7bca1cf2c2a9c13c060644
rasmusv <rasmusv@gmail.com>
2025-11-11 13:17:09 +0200

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,9 +1,7 @@
 {
   "functions": {
-    "api/*.js": {
-      "runtime": "nodejs22.x",
-      "maxDuration": 60,
-      "memory": 1024
+    "api/scrape-images.js": {
+      "runtime": "nodejs22.x"
     }
   }
 }
